ac963513f5499975bae85fd3c780a092f4681dd4
sindhunallam <117097504+sindhunallam@users.noreply.github.com>
2023-01-02 13:34:46 +0530

    Update index.js

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,1 +1,28 @@
-// Write your code here.
+import './index.css'
+
+const WinOrLose = props => {
+  const {con, playAgin} = props
+
+  const PlagainFuncall = () => {
+    playAgin()
+  }
+  return con ? (
+    <div className="resultCon">
+      <p>LOst</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png" />
+      <button type="button" onClick={PlagainFuncall}>
+        Play Again
+      </button>
+    </div>
+  ) : (
+    <div className="resultCon">
+      <p>Won</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/won-game-img.png" />
+      <button type="button" onClick={PlagainFuncall}>
+        Play Again
+      </button>
+    </div>
+  )
+}
+
+export default WinOrLose
